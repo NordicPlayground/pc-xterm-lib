b06ee9a768ae2ae091f75c7dc0b1abefeb92baee
Dee Roberts <deerob4@gmail.com>
2021-01-11 14:14:07 +0100

    Add library and README

---
+++ README.md
@@ -0,0 +1,57 @@
+# pc-xterm-lib
+
+This library implements a set of useful features for a rich terminal experience. It is implemented as a modular set of addons for the main [XTerm.js library](https://github.com/xtermjs/xterm.js), so it can be used by any application which uses XTerm.js and would benefit from a terminal with these features.
+
+It should not be considered remotely production ready; there are lots of bugs and missing features, and a lot of polish needed before it can be used in a real application.
+
+## Features
+
+-   [x] Autocompletion
+-   [x] Copy and paste support
+-   [x] Command history
+-   [x] Optional timestamps
+-   [ ] Contextual information on hover
+-   [ ] Custom styling of defined text / input via regex
+-   [ ] Probably lots more
+
+## Architecture
+
+Please see the diagram below and [this presentation](https://nordicsemi.sharepoint.com/:p:/r/sites/Wayland-MAG/Shared%20Documents/General/XTerm_Presentation.pptx?d=wa4a0f7a2b17b4cf5835d328a0908b2cd&csf=1&web=1&e=VyH30T) for information on how the library is structured.
+
+![architecture_no_alpha](docs/architecture.png)
+
+## Usage
+
+Since the library is a normal XTerm addon, usage is as straightforward as initialising it and passing it to your terminal's `loadAddon` method:
+
+```typescript
+import { Terminal } from 'xterm';
+import { NrfTerminalCommander, NrfTerminalConfig } from 'pc-xterm-lib';
+
+const config: NrfTerminalConfig = {
+    completions: [
+        {
+            name: 'my_custom_command',
+            description: 'Does something interesting',
+        },
+    ],
+    commands: {
+        my_custom_command: () => console.log('Doing something...'),
+    },
+    prompt: 'AT[:lineCount]>',
+    showTimestamps: true,
+};
+
+const nrfTerminalCommander = new NrfTerminalCommander(config);
+
+const terminal = new Terminal();
+terminal.loadAddon(nrfTerminalCommander);
+```
+
+## Feedback
+
+Please report issues to the Wayland team.
+
+## License
+
+See the [LICENSE](LICENSE) file for details.
---
+++ src/NrfTerminalCommander.ts
@@ -0,0 +1,276 @@
+import { Terminal, ITerminalAddon } from 'xterm';
+import * as ansi from 'ansi-escapes';
+
+import Prompt from './Prompt';
+import HistoryAddon from './addons/HistoryAddon';
+import TimestampAddon from './addons/TimestampAddon';
+import CopyPasteAddon from './addons/CopyPasteAddon';
+import AutocompleteAddon, { Completion } from './addons/AutocompleteAddon';
+
+import { charCode, CharCodes, isMac } from './utils';
+import HoverAddon, { HoverMetadata } from './addons/HoverAddon';
+
+export interface KeyEvent {
+    key: string;
+    domEvent: KeyboardEvent;
+}
+
+export type OutputListener = (output: string) => void;
+
+export interface NrfTerminalConfig {
+    prompt: string;
+    completions: Completion[];
+    commands: { [name: string]: () => void };
+    hoverMetadata: HoverMetadata[];
+}
+
+/**
+ * Contains logic and control code for the most common terminal tasks,
+ * like typing, moving the caret, and running commands.
+ *
+ * Some of this logic is exposed as public methods, allowing addons
+ * to make use of it.
+ *
+ * Those same addons are loaded as part of this addon's activation
+ * process, so this class can be thought of as the "root" of the
+ * addon tree.
+ */
+export default class NrfTerminalCommander implements ITerminalAddon {
+    #terminal!: Terminal;
+    #prompt: Prompt;
+    #config: NrfTerminalConfig;
+    #historyAddon!: HistoryAddon;
+    autocompleteAddon!: AutocompleteAddon;
+
+    #lineSpan = 0;
+    #lineCount = 1;
+    #outputValue = '';
+
+    #registeredCommands: { [command: string]: () => void } = {};
+    #outputListeners: ((output: string) => void)[] = [];
+
+    constructor(config: NrfTerminalConfig) {
+        this.#config = config;
+        this.#prompt = new Prompt(this, config.prompt);
+    }
+
+    public activate(terminal: Terminal) {
+        this.#terminal = terminal;
+
+        const historyAddon = new HistoryAddon(this);
+        this.#historyAddon = historyAddon;
+        this.#terminal.loadAddon(historyAddon);
+
+        const timestampAddon = new TimestampAddon(this);
+        this.#terminal.loadAddon(timestampAddon);
+
+        const copyPasteAddon = new CopyPasteAddon(this);
+        this.#terminal.loadAddon(copyPasteAddon);
+
+        const autocompleteAddon = new AutocompleteAddon(
+            this,
+            this.#config.completions
+        );
+        this.#terminal.loadAddon(autocompleteAddon);
+        this.autocompleteAddon = autocompleteAddon;
+
+        const hoverAddon = new HoverAddon(this, []);
+        this.#terminal.loadAddon(hoverAddon);
+
+        this.#terminal.onKey(this.onKey.bind(this));
+        this.#terminal.onData(this.onData.bind(this));
+
+        this.registerCommand('toggle_timestamps', () => {
+            timestampAddon.toggleTimestamps();
+        });
+
+        this.registerCommand('show_history', () => {
+            console.log(historyAddon.history);
+        });
+
+        this.registerCommand('clear_history', () => {
+            historyAddon.clearHistory();
+        });
+    }
+
+    public dispose() {
+        return false;
+    }
+
+    /**
+     * The value of the current line.
+     */
+    public get output() {
+        return this.#outputValue;
+    }
+
+    private set _output(newOutput: string) {
+        this.#outputValue = newOutput;
+        this.#outputListeners.forEach(l => l(this.output));
+    }
+
+    /**
+     * The number of lines spanned by the current command.
+     */
+    public get lineSpan() {
+        return this.#lineSpan;
+    }
+
+    public get lineCount() {
+        return this.#lineCount;
+    }
+
+    public get prompt() {
+        return this.#prompt;
+    }
+
+    /**
+     * Registers the given `command` in the terminal, such that when it is
+     * executed `callback` is run.
+     * @param command The command to listen for.
+     * @param callback The function to run when the command is given.
+     */
+    private registerCommand(command: string, callback: () => void): void {
+        this.#registeredCommands[command] = callback;
+    }
+
+    /**
+     * Registers a function that will be called whenever the output changes,
+     * with the new output value.
+     * @param listener The function to call when the output changes.
+     */
+    public registerOutputListener(listener: (output: string) => void): void {
+        this.#outputListeners.push(listener);
+    }
+
+    /**
+     * Removes the command currently being entered into the buffer
+     * and replaces it with `newCommand`.
+     * @param newCommand The command to write to the screen.
+     */
+    public replaceInputWith(newCommand: string): void {
+        this.clearInput();
+        this.#terminal.write(newCommand);
+        this._output = newCommand;
+    }
+
+    /**
+     * Returns `true` if the cursor is placed at the beginning of
+     * the line (i.e. right after the prompt), otherwise `false`.
+     */
+    public atBeginningOfLine(): boolean {
+        const buffer = this.#terminal.buffer.active;
+        return buffer.cursorX <= this.#prompt.length - 2;
+    }
+
+    /**
+     * Returns `true` if the cursor is placed at the end of
+     * the line (i.e. one character after the final one typed),
+     * otherwise `false`.
+     */
+    public atEndOfLine(): boolean {
+        const maxRightCursor = this.#prompt.length - 2 + this.output.length;
+        const buffer = this.#terminal.buffer.active;
+        return buffer.cursorX >= maxRightCursor;
+    }
+
+    /**
+     * Removes all the typed characters on the current line, and
+     * moves the cursor to the beginning.
+     */
+    public clearInput(): void {
+        const charsToDelete = this.output.length - 1;
+        for (let i = 0; i <= charsToDelete; i += 1) {
+            this.backspace();
+        }
+    }
+
+    private backspace(): void {
+        if (!this.atBeginningOfLine()) {
+            this.#terminal.write('\b \b');
+            this._output = this.output.slice(0, this.output.length - 1);
+        }
+    }
+
+    private moveCaretLeft(): void {
+        if (!this.atBeginningOfLine()) {
+            this.#terminal.write(ansi.cursorBackward(1));
+        }
+    }
+
+    private moveCaretRight(): void {
+        if (!this.atEndOfLine()) {
+            this.#terminal.write(ansi.cursorForward(1));
+        }
+    }
+
+    private runCommand(): void {
+        if (this.output.trim().length) {
+            const callback = this.#registeredCommands[this.output];
+            if (callback) {
+                callback();
+            }
+        }
+
+        this.breakCurrentCommand();
+    }
+
+    /**
+     * Prints a new prompt and removes the currently entered
+     * text. Useful whenever a new line of input needs to be
+     * started, i.e. because a command was just run.
+     */
+    private breakCurrentCommand() {
+        this.#terminal.write(this.#prompt.value);
+        this.#historyAddon.resetCursor();
+        this._output = '';
+        this.#lineCount += 1;
+    }
+
+    private onData(data: string): void {
+        switch (charCode(data)) {
+            case CharCodes.CTRL_C:
+            case CharCodes.ARROW_KEY:
+                return;
+
+            case CharCodes.BACKSPACE:
+                return this.backspace();
+
+            case CharCodes.LF:
+                if (!this.autocompleteAddon.isVisible) {
+                    return this.runCommand();
+                }
+        }
+
+        this._output = this.output + data;
+        this.updateLineSpan();
+        this.autocompleteAddon.enable();
+        this.#terminal.write(data);
+    }
+
+    private onKey(e: KeyEvent): void {
+        switch (e.domEvent.code) {
+            case 'ArrowLeft': {
+                return this.moveCaretLeft();
+            }
+
+            case 'ArrowRight': {
+                return this.moveCaretRight();
+            }
+
+            case 'KeyC': {
+                if (!e.domEvent.ctrlKey) break;
+                // On Windows and Linux, pressing Ctrl-C when text is selected
+                // should copy that text rather than breaking the line.
+                if (!isMac() && this.#terminal.getSelection().length) break;
+                this.breakCurrentCommand();
+                break;
+            }
+        }
+    }
+
+    private updateLineSpan() {
+        const delta = this.#terminal.cols - this.#prompt.length;
+        this.#lineSpan = Math.floor(this.output.length / delta);
+    }
+}
