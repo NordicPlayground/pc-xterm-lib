95380a080ecb448491225556ab824ff9206eb653
Riphiphip <43417915+Riphiphip@users.noreply.github.com>
2021-03-05 14:40:18 +0100

    Configure to build as module
    Allows loading of code in vscode webview. Still some strange behaviour, but this should be a starting point.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const config = {
     entry: './src/index.ts',
     target: 'web',
     output: {
         filename: 'index.js',
+        library: 'pcXtermLib',
+        libraryTarget:'umd',
         path: path.join(__dirname, 'dist'),
     },
     resolve: {
         extensions: ['.ts', '.js'],
     },
+    plugins: [
+        // fix "process is not defined" error
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+        }),
+      ],
     module: {
         rules: [
             {
