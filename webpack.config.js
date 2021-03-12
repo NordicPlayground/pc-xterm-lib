dc2855e5c9bbf782b93ce868b77e6f6707fa3b99
Dee Roberts <dee.roberts@nordicsemi.no>
2021-03-12 09:02:11 +0100

    Add distributable build (#6)
    * Add distributable build
    
    * Configure to build as module
    
    Allows loading of code in vscode webview. Still some strange behaviour, but this should be a starting point.
    
    * Change libraryTarget
    
    This change allows the use of the library by refering to the variable PcXtermLib without imports.
    
    * Remove source map generation
    
    * Update build artifacts
    
    Should these even be committed?
    
    * Update dist
    
    Co-authored-by: Riphiphip <43417915+Riphiphip@users.noreply.github.com>

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const config = {
+    entry: './src/index.ts',
+    target: 'web',
+    output: {
+        filename: 'index.js',
+        library: 'PcXtermLib',
+        libraryTarget:'var',
+        path: path.join(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    plugins: [
+        // fix "process is not defined" error
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+        }),
+      ],
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: [/node_modules/],
+                use: [
+                    {
+                        loader: 'ts-loader',
+                    },
+                ],
+            },
+        ],
+    },
+};
+
+module.exports = config;
